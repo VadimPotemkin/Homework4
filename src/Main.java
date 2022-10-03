9a8bf8b50e401d067dba8e2f0aa682d8d602f519
Юля <potemkin.vg@yandex.ru>
2022-10-03 22:40:44 +0300

    Задание выполнено.

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        byte age = 18;
+        if (age >= 18) {
+            System.out.println("Поздравляем вас с совершеннолетием!");
+        }
+
+
+        if (age < 18) {
+            System.out.println("Возраст совершеннолетия ещё не наступил, и нужно немного подождать.");
+        }
+    }
+}
